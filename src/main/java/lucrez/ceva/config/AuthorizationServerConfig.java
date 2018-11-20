a142edd6236e1a53e38f08e92ae3c53b7dae45c6
Pinte Laurentiu <pinte.laurentiu@yahoo.com>
2018-11-20 17:56:57 +0200

    hello no db

--- src/main/java/lucrez/ceva/config/AuthorizationServerConfig.java
+++ src/main/java/lucrez/ceva/config/AuthorizationServerConfig.java
@@ -25,20 +25,42 @@ import javax.sql.DataSource;
 @EnableAuthorizationServer
 @RequiredArgsConstructor(onConstructor=@__({@Autowired}))
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
-
-    @Qualifier("dataSource")
-    private final @NonNull DataSource dataSource;
+	private static InMemoryTokenStore store;
+//    @Qualifier("dataSource")
+//    private final @NonNull DataSource dataSource;
 	private final @NonNull AuthenticationManager authenticationManager;
 	private final @NonNull PasswordEncoder oauthClientPasswordEncoder;
 
-    public TokenStore tokenStore(){
-	    return new JdbcTokenStore(dataSource);
+	static final String GRANT_TYPE_PASSWORD = "password";
+	static final String AUTHORIZATION_CODE = "authorization_code";
+	static final String REFRESH_TOKEN = "refresh_token";
+	static final String IMPLICIT = "implicit";
+	static final String SCOPE_READ = "read";
+	static final String SCOPE_WRITE = "write";
+	static final String TRUST = "trust";
+	static final int ACCESS_TOKEN_VALIDITY_SECONDS = 60 * 60;
+	static final int FREFRESH_TOKEN_VALIDITY_SECONDS = 6*60*60;
+
+	@Bean
+    public static TokenStore tokenStore(){
+		if (store == null)
+			store = new InMemoryTokenStore();;
+	    return store;
+//    	return new JdbcTokenStore(dataSource);
     }
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer configurer) throws Exception {
-		configurer.jdbc(dataSource);
-	}
+//		configurer.jdbc(dataSource);
+		configurer
+				.inMemory()
+				.withClient("IOS-client")
+				.secret("$2a$04$I9Q2sDc4QGGg5WNTLmsz0.fvGv3OjoZyj81PrSFyGOqMphqfS2qKu")
+				.authorizedGrantTypes(GRANT_TYPE_PASSWORD, AUTHORIZATION_CODE, REFRESH_TOKEN, IMPLICIT )
+				.scopes(SCOPE_READ, SCOPE_WRITE, TRUST)
+				.accessTokenValiditySeconds(ACCESS_TOKEN_VALIDITY_SECONDS).
+						refreshTokenValiditySeconds(FREFRESH_TOKEN_VALIDITY_SECONDS);;
+    }
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
--- src/main/java/lucrez/ceva/persistence/UserRoleRepo.java
+++ src/main/java/lucrez/ceva/persistence/UserRoleRepo.java
@@ -1,15 +1,15 @@
-package lucrez.ceva.persistence;
-
-import lucrez.ceva.model.UserRole;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-@Repository
-public interface UserRoleRepo extends JpaRepository<UserRole, Long> {
-    @Query("SELECT ur from userRole ur join ur.user.userLogin ul where ul.id = :userLoginId")
-    List<UserRole> getAllByUserLoginId(@Param("userLoginId") long id);
-}
+//package lucrez.ceva.persistence;
+//
+//import lucrez.ceva.model.UserRole;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.repository.query.Param;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.List;
+//
+//@Repository
+//public interface UserRoleRepo extends JpaRepository<UserRole, Long> {
+//    @Query("SELECT ur from userRole ur join ur.user.userLogin ul where ul.id = :userLoginId")
+//    List<UserRole> getAllByUserLoginId(@Param("userLoginId") long id);
+//}
